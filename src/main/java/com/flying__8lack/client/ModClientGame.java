6c221dac291f5fc9c2fd74c25b7bd78243347638
flying_8lack <megacryleon@gmail.com>
2025-04-23 22:36:03 +0300

    Implemented a basic gui HUD

--- src/main/java/com/flying__8lack/client/ModClientGame.java
+++ src/main/java/com/flying__8lack/client/ModClientGame.java
@@ -1,7 +1,6 @@
 package com.flying__8lack.client;
 
 import com.flying__8lack.client.hud.BasicHUD;
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -11,9 +10,11 @@ import static com.flying__8lack.advancedmovementmod.MODID;
 
 @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public class ModClientGame {
+
+    //This event will render gui after the HUD was rendered (post)
     @SubscribeEvent
     public static void ClientSetUp(RenderGuiEvent.Post event){
-        BasicHUD.getInstance().render(event.getGuiGraphics(), 60, 60, 0);
+        BasicHUD.getInstance().render(event.getGuiGraphics());
 
 
     }
