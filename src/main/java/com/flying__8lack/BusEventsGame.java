53534df6395e23c4aa1b737fe81b5ca9bd874cde
flying_8lack <megacryleon@gmail.com>
2025-04-25 00:37:20 +0300

    Reorganised Bus Events

--- src/main/java/com/flying__8lack/BusEventsGame.java
+++ src/main/java/com/flying__8lack/BusEventsGame.java
@@ -1,15 +1,17 @@
 package com.flying__8lack;
 
 import com.flying__8lack.attachment.ModAttachment;
-import com.flying__8lack.client.hud.BasicHUD;
+import com.flying__8lack.blocks.BlockPipe;
+import com.flying__8lack.network.NetworkManager;
+import com.flying__8lack.network.PipeNetwork;
 import com.flying__8lack.network.PlayerData;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
-import net.neoforged.neoforge.event.entity.living.FinalizeSpawnEvent;
-import net.neoforged.neoforge.event.tick.PlayerTickEvent;
-import net.neoforged.neoforge.event.tick.ServerTickEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 import static com.flying__8lack.advancedmovementmod.MODID;
@@ -18,6 +20,16 @@ import static com.flying__8lack.advancedmovementmod.getLog;
 @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.GAME)
 public class BusEventsGame {
 
+    @SubscribeEvent
+    public static void onBlockBreak(BlockEvent.BreakEvent event){
+        if(event.getState().getBlock() instanceof BlockPipe b){
+            BlockPos pos = event.getPos();
+            PipeNetwork net = NetworkManager.getInstance().networks().get(pos);
+            if(net != null){
+                net.removeFromNetwork(pos, (Level) event.getLevel());
+            }
+        }
+    }
 
     @SubscribeEvent
     private static void onPlayerJoin(EntityJoinLevelEvent event){
