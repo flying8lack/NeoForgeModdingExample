ef0533f2fcc8506a783a26abd6bb9cfdac173095
flying_8lack <megacryleon@gmail.com>
2025-04-24 07:47:24 +0300

    Data is synced on players joining the world.

--- src/main/java/com/flying__8lack/BusEventsGame.java
+++ src/main/java/com/flying__8lack/BusEventsGame.java
@@ -1,11 +1,16 @@
 package com.flying__8lack;
 
+import com.flying__8lack.attachment.ModAttachment;
 import com.flying__8lack.client.hud.BasicHUD;
+import com.flying__8lack.network.PlayerData;
 import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.living.FinalizeSpawnEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 
 import static com.flying__8lack.advancedmovementmod.MODID;
 import static com.flying__8lack.advancedmovementmod.getLog;
@@ -13,11 +18,15 @@ import static com.flying__8lack.advancedmovementmod.getLog;
 @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.GAME)
 public class BusEventsGame {
 
+
     @SubscribeEvent
     private static void onPlayerJoin(EntityJoinLevelEvent event){
         if(!event.getLevel().isClientSide()) {
             if (event.getEntity() instanceof ServerPlayer p) {
                 getLog().debug("PLAYER JOINED: {}", p.getUUID());
+                //BasicHUD.PowerList.putIfAbsent(p.getGameProfile().getName(), p.getData(ModAttachment.PLAYER_POWER));
+
+                PacketDistributor.sendToPlayer(p, new PlayerData(p.getData(ModAttachment.PLAYER_POWER)));
             }
 
         }
